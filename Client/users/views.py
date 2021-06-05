7867baf05f0a79481b2303e0bd349aebd8eeba64
amith-jiji <amithjmadathil@gmail.com>
2021-06-05 22:33:05 +0530

    Report Page

--- Client/users/views.py
+++ Client/users/views.py
@@ -64,6 +64,5 @@ def reportView(request, id):
     if data is None:
         data = {}
     else:
-        data = [('/'.join(i[0][:10].split('_')), ':'.join(i[0][11:].split('_')), i[1][i[0]]) for i in data][
-               :min(30, len(data))]
+        data = [('/'.join(i[0][:10].split('_')), ':'.join(i[0][11:].split('_')), i[1][i[0]]) for i in data]
     return render(request, 'report.html', {'id': id, 'data': data})
