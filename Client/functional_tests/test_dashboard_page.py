9a68858628325c2c78f045c4e9af27ef12fe813f
amith-jiji <amithjmadathil@gmail.com>
2021-06-09 13:19:56 +0530

    Dashboard Test

---
+++ Client/functional_tests/test_dashboard_page.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+
+
+class TestProjectLoginPage(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path='functional_tests/geckodriver.exe')
+        self.browser.get(self.live_server_url + reverse('dashboard'))
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_dashboard_page_css_property(self):
+        self.assertEquals(self.browser.find_element_by_tag_name('body').value_of_css_property('background-color'), 'rgb(187, 186, 232)')
+
+    def test_camera1_button(self):
+        add_url = self.live_server_url + reverse('camera', args=[1])
+        self.browser.find_element_by_id('camera1-button').click()
+        self.assertEquals(
+            self.browser.current_url, add_url
+        )
+
+    def test_camera2_button(self):
+        add_url = self.live_server_url + reverse('camera', args=[2])
+        self.browser.find_element_by_id('camera2-button').click()
+        self.assertEquals(
+            self.browser.current_url, add_url
+        )
+
+    def test_sign_out(self):
+        self.browser.find_element_by_class_name('nav-link').click()
+        self.assertEquals(
+            self.browser.current_url, self.live_server_url+reverse('login')
+        )
---
+++ Client/functional_tests/test_login_page.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+
+
+class TestProjectLoginPage(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path='functional_tests/geckodriver.exe')
+        self.browser.get(self.live_server_url)
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_login_page_css_property(self):
+        self.assertEquals(self.browser.find_element_by_class_name('box').value_of_css_property('position'), 'absolute')
+
+    def test_submit_button_invalid_login(self):
+        self.browser.find_element_by_tag_name('button').click()
+        self.assertEquals(
+            self.browser.find_element_by_tag_name('p').text, "Invalid Credentials!!Please Check your Data"
+        )
+
+    def test_submit_button_valid_login(self):
+        add_url = self.live_server_url + reverse('dashboard')
+        self.browser.find_element_by_id('Email').send_keys("Sdistancemonitoring@gmail.com")
+        self.browser.find_element_by_id('Password').send_keys("Sdistance@cs8a")
+        self.browser.find_element_by_tag_name('button').click()
+        self.assertEquals(
+            self.browser.current_url, add_url
+        )
